e55b802bf9630011fc3f4189fe14743740b00738
Nildo <nildo@webstack.com.br>
2021-09-27 10:22:22 -0300

    Criado projeto e adicioando arquivos ao atom-ide-w3css

---
+++ spec/atom-ide-w3css-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomIdeW3cssView from '../lib/atom-ide-w3css-view';
+
+describe('AtomIdeW3cssView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
